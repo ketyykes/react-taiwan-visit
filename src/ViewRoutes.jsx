c9f2287773ae2e9c659fca1dd8c255850ef99031
ketyykes <xi4869@gmail.com>
2022-09-04 17:15:45 +0800

    change route

--- src/ViewRoutes.jsx
+++ src/ViewRoutes.jsx
@@ -9,7 +9,7 @@ import NotFound from "./pages/NotFound/NotFound"
 const ViewRoutes = () => {
     return (
         <Routes>
-            <Route path="/homepage/" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/search/:visitType/:city/:page" element={<Search />} />
             <Route path="/detail/:visitType" element={<Detail />} />
             <Route path="/infinite/ScenicSpot/all/:page" element={<Infinite />} />

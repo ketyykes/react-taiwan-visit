1a1d0e0657224c75f0cbebf6639a74772ffdc6a8
danny <xi4869@gmail.com>
2024-04-06 00:04:59 +0800

    Refactor useGetPlaceData and useRandomPlaceQuery hooks

--- src/hook/useGetPlaceData.js
+++ src/hook/useGetPlaceData.js
@@ -3,34 +3,34 @@ import { useEffect, useState } from "react";
 import { visitInstance } from "../API/visit_api.js";
 
 export default function useGetPlaceData(pathUrl, params, city = "all") {
-  const [place, setPlace] = useState([]);
-  const {
-    token: {
-      tokenData: { data: { access_token, token_type } = {} },
-    },
-  } = useSelector((state) => state);
-  params += "&$format=JSON";
-  const cityPath = city === "all" ? "" : "/" + city;
-  useEffect(() => {
-    if (access_token) {
-      (async () => {
-        const config = {
-          method: "GET",
-          url: `/Tourism/${pathUrl + cityPath}?${params}`,
-          headers: {
-            Authorization: `${token_type} ${access_token}`,
-          },
-        };
-        console.log(config);
-        try {
-          const { data } = await visitInstance(config);
-          setPlace(data);
-        } catch (error) {
-          console.log(error);
-          return null;
-        }
-      })();
-    }
-  }, [params, cityPath, pathUrl, access_token, token_type]);
-  return place;
+	const [place, setPlace] = useState(null);
+	const {
+		token: {
+			tokenData: { data: { access_token, token_type } = {} },
+		},
+	} = useSelector((state) => state);
+	params += "&$format=JSON";
+	const cityPath = city === "all" ? "" : "/" + city;
+	useEffect(() => {
+		if (access_token) {
+			(async () => {
+				const config = {
+					method: "GET",
+					url: `/Tourism/${pathUrl + cityPath}?${params}`,
+					headers: {
+						Authorization: `${token_type} ${access_token}`,
+					},
+				};
+				// console.log(config);
+				try {
+					const { data } = await visitInstance(config);
+					setPlace(data);
+				} catch (error) {
+					console.log(error);
+					return [];
+				}
+			})();
+		}
+	}, [params, cityPath, pathUrl, access_token, token_type]);
+	return place;
 }
--- src/hook/useRandomPlaceQuery.js
+++ src/hook/useRandomPlaceQuery.js
@@ -1,39 +1,43 @@
 import {
-  SCENICSPOT_MORE_QUERY,
-  ACTIVITY_MORE_QUERY,
-  RESTAURANT_MORE_QUERY,
-  HOTEL_MORE_QUERY,
+	SCENICSPOT_MORE_QUERY,
+	ACTIVITY_MORE_QUERY,
+	RESTAURANT_MORE_QUERY,
+	HOTEL_MORE_QUERY,
 } from "../assets/visitTypeQuery.js";
 import useGetPlaceData from "./useGetPlaceData";
 
 function makeRandomNumberArray(range, quantity) {
-  if (range < quantity) return [];
-  let set = new Set();
-  for (let index = 0; index < quantity; index++) {
-    let randomNumber = Math.floor(Math.random() * range);
-    set.has(randomNumber) ? index-- : set.add(randomNumber);
-  }
-  return Array.from(set);
+	if (range < quantity) return [];
+	let set = new Set();
+	for (let index = 0; index < quantity; index++) {
+		let randomNumber = Math.floor(Math.random() * range);
+		set.has(randomNumber) ? index-- : set.add(randomNumber);
+	}
+	return Array.from(set);
 }
 
 export default function useRandomPlaceQuery(visitType, quantity) {
-  let paramsQuery;
-  if (visitType === "ScenicSpot") {
-    paramsQuery = SCENICSPOT_MORE_QUERY;
-  }
-  if (visitType === "Activity") {
-    paramsQuery = ACTIVITY_MORE_QUERY;
-  }
-  if (visitType === "Restaurant") {
-    paramsQuery = RESTAURANT_MORE_QUERY;
-  }
-  if (visitType === "Hotel") {
-    paramsQuery = HOTEL_MORE_QUERY;
-  }
-  const allData = useGetPlaceData(visitType, paramsQuery);
-  const randomNumberArray = makeRandomNumberArray(allData.length, quantity);
-  const returnData = randomNumberArray.map(
-    (randomNumber) => (randomNumber = allData[randomNumber])
-  );
-  return returnData;
+	let paramsQuery;
+	if (visitType === "ScenicSpot") {
+		paramsQuery = SCENICSPOT_MORE_QUERY;
+	}
+	if (visitType === "Activity") {
+		paramsQuery = ACTIVITY_MORE_QUERY;
+	}
+	if (visitType === "Restaurant") {
+		paramsQuery = RESTAURANT_MORE_QUERY;
+	}
+	if (visitType === "Hotel") {
+		paramsQuery = HOTEL_MORE_QUERY;
+	}
+	const allData = useGetPlaceData(visitType, paramsQuery);
+	if (!allData === null) {
+		const randomNumberArray = makeRandomNumberArray(allData.length, quantity);
+		const returnData = randomNumberArray.map(
+			(randomNumber) => (randomNumber = allData[randomNumber])
+		);
+		return returnData;
+	} else {
+		return [];
+	}
 }

89d710f1a0f589bf8eec0b71522a828b50815d17
danny <xi4869@gmail.com>
2024-04-06 00:05:34 +0800

    Add Loading component and apply fade-in animation to article section

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import { useParams, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 import styles from './search.module.scss';
 import { useToggle, useGetPlaceData } from '../../hook'
-import { Footer, Card, Aside, Header, Pagination } from '../../component'
+import { Footer, Card, Aside, Header, Pagination, Loading } from '../../component'
 import ThemeCardContentByVisitType from '../../component/ThemeCardContentByVisitType';
 const Search = () => {
     const {
         display_none, display_block,
-        container, article, wrap_card, title_type, no_result } = styles;
+        container, article, wrap_card, title_type, no_result, fade_in } = styles;
     const [menuValue, menuValueFunction] = useToggle(false);
     const [searchParams] = useSearchParams();
     const params = useParams();
     const { visitType, city, page } = params;
     const data = useGetPlaceData(visitType, searchParams.toString(), city);
-    const renderData = data.slice((page - 1) * 12, page * 12);
     const CardContent = ThemeCardContentByVisitType[visitType];
     let { title } = useSelector(state => state.selectResult);
+
+    const renderData = data?.slice((page - 1) * 12, page * 12);
+
+
+
     return (
         <div className={container}>
             <Aside
@@ -28,34 +33,39 @@ const Search = () => {
                     {title ||= "搜尋結果"}
                 </h2>
             </Header>
-            <article className={`${article} ${menuValue ? display_none : display_block}`}>
-                <div className={wrap_card}>
-                    {
-                        renderData.length !== 0 ? (
-                            renderData.map(
-                                (place, index) =>
-                                (
-                                    <Card key={index}
-                                        visitType={visitType}
-                                        placeDatum={place}>
-                                        <CardContent placeDatum={place} />
-                                    </Card>
-                                )
-                            )
-                        ) : (<span className={no_result}>找不到你所搜尋的內容</span>)
-                    }
-                </div>
-                {
-                    renderData.length !== 0 && (<Pagination
-                        route="search"
-                        city={city}
-                        currentPage={+page}
-                        itemAmount={data.length}
-                        visitType={visitType}
-                    />)
-                }
 
-            </article>
+            {data === null ? <Loading />
+                : <article className={`${article} ${menuValue ? display_none : display_block}`}>
+                    <div className={fade_in}>
+                        <div className={wrap_card}>
+                            {
+
+                                renderData.length !== 0 ? (
+                                    renderData.map(
+                                        (place, index) =>
+                                        (
+                                            <Card key={index}
+                                                visitType={visitType}
+                                                placeDatum={place}>
+                                                <CardContent placeDatum={place} />
+                                            </Card>
+                                        )
+                                    )
+                                ) : (<span className={no_result}>找不到你所搜尋的內容</span>)
+                            }
+                        </div>
+                        {
+                            renderData.length !== 0 && (<Pagination
+                                route="search"
+                                city={city}
+                                currentPage={+page}
+                                itemAmount={data.length}
+                                visitType={visitType}
+                            />)
+                        }
+                    </div>
+                </article>
+            }
             <Footer menuValue={menuValue} />
         </div >
     )
